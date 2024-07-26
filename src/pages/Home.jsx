5c2ea480402960ea5856767e00e58849c6d28af1
zhikangxie107 <zhikangxie107@gmail.com>
2024-07-26 00:51:09 -0400

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+// State
+import { useGetUsersQuery } from "../app/apiSlice";
+// Components
+import Hero from "../components/Hero";
+import AboutMe from "../components/AboutMe";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import BackToTop from "../components/BackToTop";
+// Config
+import { filteredProjects, moreInfo } from "../config";
+// Utils
+import { updateTitle } from "../utils";
+
+// #region component
+const Home = () => {
+  const { data: userData } = useGetUsersQuery();
+
+  React.useEffect(() => {
+    updateTitle(`${userData.name} | Portfolio`);
+  }, [userData]);
+
+  return (
+    <>
+      <Hero name={userData.name} />
+      <main>
+        <AboutMe
+          avatar_url={userData.avatar_url}
+          bio={
+            "Hello! I'm Zhi, a rising junior majoring in Computer Science at the University at Buffalo. I have a passion for software development, particularly in Full Stack. Through various projects and collaborations, I've developed a strong foundation in web development and database management. I'm eager to apply my skills in real-world settings and connect with like-minded professionals. Let's connect!"
+          }
+          moreInfo={""}
+        />
+        <Skills />
+        <Projects filteredProjects={filteredProjects} />
+        <Contact />
+      </main>
+      <BackToTop />
+    </>
+  );
+};
+// #endregion
+
+export default Home;
